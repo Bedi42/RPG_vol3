78146bba303f9208616100043b95ec0432296720
Bedi42 <krzyschodak@wp.pl>
2022-06-28 22:45:56 +0200

    authentication v2

---
+++ rpg-app/src/components/App.js
@@ -0,0 +1,107 @@
+// import { useState, useRef } from "react";
+// import {
+//   createUserWithEmailAndPassword,
+//   onAuthStateChanged,
+//   signOut,
+//   signInWithEmailAndPassword,
+// } from "firebase/auth";
+// import { auth } from "./Firebase.config.js";
+
+// function App() {
+//   const [user, setUser] = useState({});
+//   const registerEmailRef = useRef();
+//   const registerPasswordRef = useRef();
+//   const loginEmailRef = useRef();
+//   const loginPasswordRef = useRef();
+
+//   onAuthStateChanged(auth, (currentUser) => {
+//     setUser(currentUser);
+//   });
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     register();
+//     login();
+//   };
+
+//   const register = async () => {
+//     try {
+//       const user = await createUserWithEmailAndPassword(
+//         auth,
+//         registerEmailRef.current.value,
+//         registerPasswordRef.current.value
+//       );
+//       console.log(user);
+//     } catch (error) {
+//       console.log(error.message);
+//     }
+//   };
+
+//   const login = async () => {
+//     try {
+//       const user = await signInWithEmailAndPassword(
+//         auth,
+//         loginEmailRef.current.value,
+//         loginPasswordRef.current.value
+//       );
+//       console.log(user);
+//     } catch (error) {
+//       console.log(error.message);
+//     }
+//   };
+
+//   const logout = async () => {
+//     await signOut(auth);
+//   };
+
+//   return (
+//     <div className="App">
+//       <form onSubmit={handleSubmit}>
+//         <h3>Register</h3>
+//         <input type="text" placeholder="Email..." ref={registerEmailRef} />
+//         <input
+//           type="text"
+//           placeholder="Password..."
+//           ref={registerPasswordRef}
+//         />
+//         <button type="submit">Create User</button>
+//       </form>
+
+//       <form onSubmit={handleSubmit}>
+//         <h3>Login</h3>
+//         <input type="text" placeholder="Email..." ref={loginEmailRef} />
+//         <input type="text" placeholder="Password..." ref={loginPasswordRef} />
+//         <button type="submit">Login</button>
+//       </form>
+
+//       <div>
+//         <h4>User Logged In:</h4>
+//         {user?.email}
+
+//         <button onClick={logout}>Sing out</button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+import SignUp from "./SignUp";
+import { Container } from "react-bootstrap";
+import { AuthProvider } from "./context/AuthContext";
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="w-100" style={{ maxWidth: "450px" }}>
+          <SignUp />
+        </div>
+      </Container>
+    </AuthProvider>
+  );
+}
---
+++ rpg-app/src/components/SignUp.js
@@ -0,0 +1,64 @@
+import React, { useState, useRef } from "react";
+import { Card, Form, Button, Alert } from "react-bootstrap";
+import { useAuth } from "./context/AuthContext";
+
+export default function SignUp() {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+  const { signup } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError("Passwords are not the same");
+    }
+
+    try {
+      setError("");
+      setLoading(true);
+      await signup(emailRef.current.value, passwordRef.current.value);
+    } catch {
+      setError("Account is not created");
+    }
+    setLoading(false);
+  };
+
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Sign Up</h2>
+          {error && <Alert>{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" ref={emailRef} required />
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" ref={passwordRef} required />
+            </Form.Group>
+            <Form.Group id="password-confirm">
+              <Form.Label>Confirm the password</Form.Label>
+              <Form.Control type="password" ref={passwordConfirmRef} required />
+            </Form.Group>
+            <Button
+              disabled={loading}
+              type="submit"
+              className="w-100 text-center mt-2"
+            >
+              Sign Up
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        Already have an account? Log In!
+      </div>
+    </>
+  );
+}
